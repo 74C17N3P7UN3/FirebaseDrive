7325a5c0aff0d0143a7f532ed8a09f6a914e71d2
74C17N3P7UN3 <leocalzavara05@gmail.com>
2023-04-20 21:02:49 +0200

    Added upload progress

--- src/components/drive/AddFileBtn.jsx
+++ src/components/drive/AddFileBtn.jsx
@@ -1,4 +1,8 @@
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
+import { useState } from 'react'
+import { ProgressBar, Toast } from 'react-bootstrap'
+import ReactDOM from 'react-dom'
+import { v4 as uuidV4 } from 'uuid'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFileUpload } from '@fortawesome/free-solid-svg-icons'
@@ -9,12 +13,22 @@ import { ROOT_FOLDER } from '../../hooks/useFolder'
 import { addDoc } from 'firebase/firestore'
 
 const AddFileBtn = ({ currentFolder }) => {
+   const [uploadingFiles, setUploadingFiles] = useState([])
+
    const { currentUser } = useAuth()
 
    const handleUpload = (e) => {
       const file = e.target.files[0]
 
       if (currentFolder == null || file == null) return
+
+      const id = uuidV4()
+      setUploadingFiles((prev) => [...prev, {
+         id: id,
+         name: file.name,
+         progress: 0,
+         error: false,
+      }])
 
       const treePath = currentFolder.path.map(e => e.name).join('/')
       const filePath = currentFolder !== ROOT_FOLDER
@@ -27,13 +41,26 @@ const AddFileBtn = ({ currentFolder }) => {
       )
 
       uploadTask.on('state_changed',
-         (snapshot) => {
-            //
+         (snap) => {
+            const progress = snap.bytesTransferred / snap.totalBytes * 100
+            setUploadingFiles((prev) => prev.map((file) => {
+               if (file.id === id)
+                  return { ...file, progress: progress }
+               return file
+            }))
          },
          (error) => {
-            //
+            console.log(error)
+
+            setUploadingFiles((prev) => prev.map((file) => {
+               if (file.id === id)
+                  return { ...file, error: true }
+               return file
+            }))
          },
          () => {
+            setUploadingFiles((prev) => prev.filter((file) => file.id !== id))
+
             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
                addDoc(database.files, {
                   createdAt: database.timestamp(),
@@ -48,17 +75,59 @@ const AddFileBtn = ({ currentFolder }) => {
    }
 
    return (
-      <label
-         className='btn btn-md btn-outline-success'
-         style={{ width: '50px', marginLeft: '10px' }}
-      >
-         <FontAwesomeIcon icon={faFileUpload} />
-         <input
-            type='file'
-            onChange={handleUpload}
-            style={{ display: 'none' }}
-         />
-      </label>
+      <>
+         <label
+            className='btn btn-md btn-outline-success'
+            style={{ width: '50px', marginLeft: '10px' }}
+         >
+            <FontAwesomeIcon icon={faFileUpload} />
+            <input
+               type='file'
+               onChange={handleUpload}
+               style={{ display: 'none' }}
+            />
+         </label>
+
+         {uploadingFiles.length > 0 && (
+            ReactDOM.createPortal(
+               <div style={{
+                  position: 'absolute',
+                  right: '1rem',
+                  bottom: '1rem',
+                  maxWidth: '250px',
+               }}>
+                  {uploadingFiles.map((file) => (
+                     <Toast key={file.id} onClose={() => {
+                        setUploadingFiles((prev) =>
+                           prev.filter(file =>
+                              file.id !== file.id
+                           )
+                        )
+                     }}>
+                        <Toast.Header
+                           closeButton={file.error}
+                           className='d-block text-truncate w-100'
+                        >
+                           {file.name}
+                        </Toast.Header>
+                        <Toast.Body>
+                           <ProgressBar
+                              animated={!file.error}
+                              variant={file.error ? 'danger' : 'primary'}
+                              now={file.error ? 100 : file.progress}
+                              label={file.error
+                                 ? 'Error'
+                                 : `${file.progress.toFixed(1)}%`
+                              }
+                           />
+                        </Toast.Body>
+                     </Toast>
+                  ))}
+               </div>,
+               document.body
+            )
+         )}
+      </>
    )
 }
 
